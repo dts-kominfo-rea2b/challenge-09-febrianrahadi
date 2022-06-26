ad1507ce1737b399779cd47ebb6367b3bbdefe3a
febrianrahadi <febriansetiyo@gmail.com>
2022-06-26 21:46:46 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 // Uncomment untuk memuat daftar kontak
 // import contacts from './data/contacts.json';
@@ -7,7 +9,14 @@ const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Header title="Call a Friend" subtitle="your friendly contact app" />
+      <Header />
+      <Contact data={contacts[0]} />
+      <Contact data={contacts[1]} />
+      <Contact data={contacts[2]} />
+      <Contact data={contacts[3]} />
+      <Contact data={contacts[4]} />
+      <Contact data={contacts[5]} />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
-const Header = () => {
+const Header = (props) => {
     return (
-        <></>
+        <div style={{width:'100%'}}>
+            <table style={{width:'100%'}}>
+                <tr>
+                    <td><h1>{ props.title }</h1></td>
+                    <td><h1>Call a Friend</h1></td>
+                </tr>
+                <tr>
+                    <td><p>{ props.subtitle }</p></td>
+                    <td><p>your friendly contact app</p></td>
+                </tr>
+                <tr>
+                    <td><hr className="hrStyle"/></td>
+                </tr>
+            </table>
+        </div>
     )
 }
 
