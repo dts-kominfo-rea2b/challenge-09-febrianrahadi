acdbcfb0c3c1414f8f163d35ae907afa41fd9efd
febrianrahadi <febriansetiyo@gmail.com>
2022-06-26 22:14:53 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from './components/Contact';
 import Header from './components/Header';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
