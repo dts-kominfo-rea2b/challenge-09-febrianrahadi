7f54e7c2df6bebbf069c564ec701c319ca6dfbc2
febrianrahadi <febriansetiyo@gmail.com>
2022-06-26 22:20:30 +0700

    commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,29 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='divStyle'>
+            <table style={{width:'100%'}}>
+                <colgroup><col style={{ width: '30%' }}/><col style={{ width: '70%' }}/></colgroup>
+                <tr>
+                    <td rowSpan={4}>
+                        <img style={{borderRadius:'50%'}} width={150} src={props.data.photo} alt="f" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colSpan={2}>{props.data.name}</td>
+                </tr>
+                <tr>
+                    <td colSpan={2}>{props.data.phone}</td>
+                </tr>
+                <tr>
+                    <td colSpan={2}>{props.data.email}</td>
+                </tr>
+            </table>
+        </div>
     )
 }
 
